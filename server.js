efed07cf7f802442403ba634f5f82bcf0e43a6a2
Ludvig Prim <ludvig@prims.se>
2024-04-04 14:56:03 +0200

    testing page
    r

--- server.js
+++ server.js
@@ -10,3 +10,20 @@ const app = express();
 const port = process.env.PORT;
 
 const server = createServer(app);
+
+server.listen(port, () => {
+    console.log(`TableturfQ is up at port ${port}`);
+});
+
+app.use(express.static('public',{extensions:['html']}));
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+app.get("/login", async (req, res) => {
+    //check log in
+    //res.sendFile(join(__dirname, "index.html"));
+})
+
+app.get("/LjovynnsTestingPage", async (req, res) => {
+    res.sendFile(join(__dirname, "LjovynnsTestingPage.html"));
+})
