82c38b5c27d4c43475e692e5afc75e2b3f149909
Ludvig Prim <ludvig@prims.se>
2024-04-08 21:23:44 +0200

    discord oauth2
    yea

--- server.js
+++ server.js
@@ -1,29 +1,79 @@
 const express = require("express");
-
+const axios = require("axios");
 const dotenv = require("dotenv");
+const url = require("url");
+const { request } = require("http");
 
-dotenv.config()
+dotenv.config();
 
-var createServer = require('http');
-
+const website_url = process.env.URL;
 const app = express();
 const port = process.env.PORT;
 
-const server = createServer(app);
-
-server.listen(port, () => {
+app.listen(port, () => {
     console.log(`TableturfQ is up at port ${port}`);
 });
 
 app.use(express.static('public',{extensions:['html']}));
-app.use(express.urlencoded({extended: true}))
-app.use(express.json())
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
 app.get("/login", async (req, res) => {
     //check log in
     //res.sendFile(join(__dirname, "index.html"));
-})
+});
 
-app.get("/LjovynnsTestingPage", async (req, res) => {
+app.get("/ljovynnspestingpage", async (req, res) => {
     res.sendFile(join(__dirname, "LjovynnsTestingPage.html"));
-})
+});
+
+app.get("/api/auth/discord/redirect", async (req, res) => {
+    const { code } = req.query;
+
+    if (!code){
+        return;
+    }
+
+    const formData = new url.URLSearchParams({
+        client_id: process.env.CLIENT_ID,
+        client_secret: process.env.CLIENT_SECRET,
+        grant_type: "authorization_code",
+        code: code.toString(),
+        redirect_uri: website_url + ":" + port + "/api/auth/discord/redirect",
+    });
+
+    const output = await axios.post("https://discord.com/api/v10/oauth2/token",
+        formData, {
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+            },
+    });
+
+    if (output.data) {
+        const access = output.data.access_token;
+
+        const userInfo = await axios.get("https://discord.com/api/v10/users/@me", {
+            headers: {
+                "Authorization": `Bearer ${access}`,
+            },
+        });
+
+        //refresh token
+
+        const requestFormData = new url.URLSearchParams({
+            client_id: process.env.CLIENT_ID,
+            client_secret: process.env.CLIENT_SECRET,
+            grant_type: "refresh_token",
+            refresh_token: output.data.refresh_token,
+        });
+
+        const refresh = await axios.post("https://discord.com/api/v10/oauth2/token",
+            requestFormData, {
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded",
+                },
+        });
+
+        //console.log(output.data, userInfo.data, refresh.data);
+    }
+});
