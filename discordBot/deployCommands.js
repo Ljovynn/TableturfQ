818b70afdd2ff71e1cf812dd3431be37b7b2f144
Ludvig Prim <ludvig@prims.se>
2024-06-13 04:24:51 +0200

    global and dev commands system

--- discordBot/deployCommands.js
+++ discordBot/deployCommands.js
@@ -16,9 +16,10 @@ const guildId = process.env.GUILD_ID;
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const commands = [];
+const devCommands = [];
 
-const folderPath = path.join(__dirname, 'commands');
-const commandFolder = fs.readdirSync(folderPath);
+var folderPath = path.join(__dirname, 'commands');
+var commandFolder = fs.readdirSync(folderPath);
 
 
 for (const file of commandFolder) {
@@ -33,16 +34,34 @@ for (const file of commandFolder) {
 	}
 }
 
+folderPath = path.join(__dirname, 'devCommands');
+commandFolder = fs.readdirSync(folderPath);
+
+
+for (const file of commandFolder) {
+    const filePath = `./devCommands/${file}`;
+    console.log(filePath);
+	let command = await import (filePath);
+	// Set a new item in the Collection with the key as the command name and the value as the exported module
+    if ('data' in command && 'execute' in command) {
+		devCommands.push(command.data.toJSON());
+	} else {
+		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+	}
+}
+
 const rest = new REST().setToken(token);
 
 (async () => {
 	try {
-		console.log(`Started refreshing ${commands.length} application (/) commands.`);
+		console.log(`Started refreshing ${commands.length} application (/) commands and ${devCommands.length} application (/) dev commands.`);
 
 		// The put method is used to fully refresh all commands in the guild with the current set
 		const data = await rest.put(
 			Routes.applicationCommands(clientId),
 			{ body: commands },
+			Routes.applicationGuildCommands(clientId, guildId),
+			{ body: devCommands },
 		);
 
 		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
--- discordBot/discordBotManager.js
+++ discordBot/discordBotManager.js
@@ -16,13 +16,26 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.commands = new Collection();
 
-const folderPath = path.join(__dirname, 'commands');
+var folderPath = path.join(__dirname, 'commands');
 const commandFolder = fs.readdirSync(folderPath);
+folderPath = path.join(__dirname, 'devCommands');
+const devCommandFolder = fs.readdirSync(folderPath);
 
 var previousDisputeMessageId;
 
 for (const file of commandFolder) {
     const filePath = `./commands/${file}`;
+	let command = await import (filePath);
+	// Set a new item in the Collection with the key as the command name and the value as the exported module
+    if ('data' in command && 'execute' in command) {
+		client.commands.set(command.data.name, command);
+	} else {
+		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+	}
+}
+
+for (const file of devCommandFolder) {
+    const filePath = `./devCommands/${file}`;
 	let command = await import (filePath);
 	// Set a new item in the Collection with the key as the command name and the value as the exported module
     if ('data' in command && 'execute' in command) {
