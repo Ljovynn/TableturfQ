71f5b5c1a7c946b202361724b6a29dd815bde282
Mike <mcondave@gmail.com>
2024-06-13 12:05:37 -0400

    Started reading and populating profile data

--- public/scripts/profile.js
+++ public/scripts/profile.js
@@ -1,10 +1,22 @@
+const userDiscordName = document.getElementById('user-discord-name');
 const userDisplayNameContent = document.getElementById('user-in-game-name');
 const userDisplayName =  document.getElementById('user-in-game-name-value');
+const userProfilePicture = document.getElementById('user-profile-picture');
 const editDisplayName = document.getElementById('user-profile-edit-name');
 const editDisplayNameForm = document.getElementById('user-profile-edit-name-form');
 const displayNameInput = document.getElementById('user-profile-name-input');
 const displayNameSubmit = document.getElementById('user-profile-name-submit');
 const editDisplayNameClose = document.getElementById('user-profile-edit-close');
+
+var user;
+var userId;
+var discordId;
+var discordAvatarHash;
+var username;
+var userInfo;
+
+setUserInfo();
+
 
 editDisplayName.addEventListener('click', (e) => {
     console.log('User clicked the display name edit button');
@@ -25,7 +37,7 @@ displayNameSubmit.addEventListener('click', (e) => {
     if ( validateDisplayName(newDisplayName) ) {
 
         data = { userDisplayName: newDisplayName };
-        response = postData('/edit-profile', data);
+        response = postData('/user/EditInGameName', data);
 
         // On successful response
         editDisplayNameForm.classList.toggle('editing');
@@ -36,6 +48,27 @@ displayNameSubmit.addEventListener('click', (e) => {
         alert('The submitted display name is invalid. Please try again.');
     }
 });
+
+async function getUserInfo() {
+    var data = {};
+    var result = await fetchData('/user/GetUserInfo');
+    return result;
+}
+
+async function setUserInfo() {
+    userInfo = await getUserInfo();
+    console.log(userInfo);
+
+    user = userInfo.user;
+    username = user.username;
+    discordId = user.discord_id;
+    discordAvatarHash = user.discord_avatar_hash;
+
+    userDiscordName.innerHTML = username;
+    avatarString = 'https://cdn.discordapp.com/avatars/' + discordId + '/' + discordAvatarHash + '.jpg';
+    userProfilePicture.src = avatarString;
+
+}
 
 function validateDisplayName(newDisplayName) {
     // Just validate the name isn't empty for now
