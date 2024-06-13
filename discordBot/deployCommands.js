b772a4d61727e424c4558617a73c4daf6bb9c2fe
Ludvig Prim <ludvig@prims.se>
2024-06-13 03:55:54 +0200

    commands not restricted to server

--- discordBot/deployCommands.js
+++ discordBot/deployCommands.js
@@ -41,7 +41,7 @@ const rest = new REST().setToken(token);
 
 		// The put method is used to fully refresh all commands in the guild with the current set
 		const data = await rest.put(
-			Routes.applicationGuildCommands(clientId, guildId),
+			Routes.applicationCommands(clientId),
 			{ body: commands },
 		);
 
