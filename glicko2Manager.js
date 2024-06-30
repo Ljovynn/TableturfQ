4b86cd907226d981bcb0d11c488e4b47e78f23a1
Ludvig Prim <ludvig@prims.se>
2024-07-01 00:50:51 +0200

    slightly lower rd

--- glicko2Manager.js
+++ glicko2Manager.js
@@ -12,7 +12,7 @@ export var settings = {
     rating : 1400,
     //rd : Default rating deviation 
     //     small number = good confidence on the rating accuracy
-    rd : 140,
+    rd : 130,
     //vol : Default volatility (expected fluctation on the player rating)
     vol : 0.06
   };
