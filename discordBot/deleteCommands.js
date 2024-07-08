110f2558d04af748bcfcd3f2499fbb676e9b20bc
Ludvig Prim <ludvig@prims.se>
2024-07-08 02:21:33 +0200

    circular dependancy workaround deploy commands

--- discordBot/deleteCommands.js
+++ discordBot/deleteCommands.js
@@ -15,11 +15,21 @@ const rest = new REST().setToken(token);
 //
 
 // for guild-based commands
-rest.delete(Routes.applicationGuildCommand(clientId, guildId, '1252580281626525809'))
+/*rest.delete(Routes.applicationGuildCommand(clientId, guildId, '1252580281626525809'))
 	.then(() => console.log('Successfully deleted guild command'))
+	.catch(console.error);*/
+
+//deleting every guild command
+rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: [] })
+	.then(() => console.log('Successfully deleted all guild commands.'))
 	.catch(console.error);
 
 // for global commands
 /*rest.delete(Routes.applicationCommand(clientId, 'commandId'))
 	.then(() => console.log('Successfully deleted application command'))
+	.catch(console.error);*/
+
+//deleting every global command
+/*rest.put(Routes.applicationCommands(clientId), { body: [] })
+	.then(() => console.log('Successfully deleted all application commands.'))
 	.catch(console.error);*/
--- discordBot/deployCommands.js
+++ discordBot/deployCommands.js
@@ -18,56 +18,57 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const commands = [];
 const guildCommands = [];
 
-var folderPath = path.join(__dirname, 'commands');
-var commandFolder = fs.readdirSync(folderPath);
+export async function Deploy(){
+	var folderPath = path.join(__dirname, 'commands');
+	var commandFolder = fs.readdirSync(folderPath);
 
-
-for (const file of commandFolder) {
-    const filePath = `./commands/${file}`;
-    console.log(filePath);
-	let command = await import (filePath);
-	// Set a new item in the Collection with the key as the command name and the value as the exported module
-    if ('data' in command && 'execute' in command) {
-		commands.push(command.data.toJSON());
-	} else {
-		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+	for (const file of commandFolder) {
+		const filePath = `./commands/${file}`;
+		console.log(filePath);
+		let command = await import (filePath);
+		// Set a new item in the Collection with the key as the command name and the value as the exported module
+		if ('data' in command && 'execute' in command) {
+			commands.push(command.data.toJSON());
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
 	}
+	
+	folderPath = path.join(__dirname, 'guildCommands');
+	commandFolder = fs.readdirSync(folderPath);
+	
+	
+	for (const file of commandFolder) {
+		const filePath = `./guildCommands/${file}`;
+		console.log(filePath);
+		let command = await import (filePath);
+		// Set a new item in the Collection with the key as the command name and the value as the exported module
+		if ('data' in command && 'execute' in command) {
+			guildCommands.push(command.data.toJSON());
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
+	
+	const rest = new REST().setToken(token);
+	
+	(async () => {
+		try {
+			console.log(`Started refreshing ${commands.length} application (/) commands and ${guildCommands.length} application (/) guild commands.`);
+	
+			// The put method is used to fully refresh all commands in the guild with the current set
+			const globalData = await rest.put(
+				Routes.applicationCommands(clientId),
+				{ body: commands },
+			);
+			const guildData = await rest.put(
+				Routes.applicationGuildCommands(clientId, guildId),
+				{ body: guildCommands },
+			);
+	
+			console.log(`Successfully reloaded ${globalData.length} application (/) commands and ${guildData.length} application (/) guild commands.`);
+		} catch (error) {
+			console.error(error);
+		}
+	})();
 }
-
-folderPath = path.join(__dirname, 'guildCommands');
-commandFolder = fs.readdirSync(folderPath);
-
-
-for (const file of commandFolder) {
-    const filePath = `./guildCommands/${file}`;
-    console.log(filePath);
-	let command = await import (filePath);
-	// Set a new item in the Collection with the key as the command name and the value as the exported module
-    if ('data' in command && 'execute' in command) {
-		guildCommands.push(command.data.toJSON());
-	} else {
-		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-	}
-}
-
-const rest = new REST().setToken(token);
-
-(async () => {
-	try {
-		console.log(`Started refreshing ${commands.length} application (/) commands and ${guildCommands.length} application (/) guild commands.`);
-
-		// The put method is used to fully refresh all commands in the guild with the current set
-		const globalData = await rest.put(
-			Routes.applicationCommands(clientId),
-			{ body: commands },
-		);
-		const guildData = await rest.put(
-			Routes.applicationGuildCommands(clientId, guildId),
-			{ body: guildCommands },
-		);
-
-		console.log(`Successfully reloaded ${globalData.length} application (/) commands and ${guildData.length} application (/) guild commands.`);
-	} catch (error) {
-		console.error(error);
-	}
-})();
