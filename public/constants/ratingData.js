810d5f2102013cb44900ad5ba07d2497dd1c6996
Ludvig Prim <ludvig@prims.se>
2024-07-17 16:22:56 +0200

    bug fix

--- public/constants/ratingData.js
+++ public/constants/ratingData.js
@@ -2,5 +2,5 @@ export const ratingHistoryOptions = Object.freeze({
     season: 0,
     day: 24 * 60 * 60 * 1000,
     week: 7 * 24 * 60 * 60 * 1000, 
-    month: 31 * 7 * 24 * 60 * 60 * 1000,
+    month: 31 * 24 * 60 * 60 * 1000,
 });
