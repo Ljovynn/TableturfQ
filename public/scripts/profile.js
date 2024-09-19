ec67ac40ae2584ba0aa17fd4f3a8a03d7e567111
Mike <mcondave@gmail.com>
2024-09-19 17:48:18 -0400

    Match history scroll improvement

--- public/scripts/profile.js
+++ public/scripts/profile.js
@@ -211,8 +211,12 @@ graphSubmit.addEventListener('click', async (e) => {
 
 window.addEventListener('scroll', async (e) => {
     // Only do this when they're on the correct tab
+    console.log('scrollY', window.scrollY);
+    console.log('screen height', screen.height);
+    console.log('1/3 screen height', screen.height/3);
+    console.log('div height', matchHistory.offsetHeight);
     if ( toggleMatchHistory.classList.contains('active') ) {
-        if ( window.scrollY + (screen.height/3) >= matchHistory.offsetHeight - 10 ) {
+        if ( window.scrollY + (screen.height/3) >= matchHistory.offsetHeight/2 ) {
             if ( !loadingMatches ) {
                 loadingMatches = true;
                 let hits = document.getElementsByClassName('match-row');
