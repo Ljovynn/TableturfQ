5c37bd256e0e4788f043a04827a9f4cb786c2567
Ludvig Prim <ludvig@prims.se>
2024-10-09 14:44:08 +0200

    bigger starting elo serach range

--- constants/queData.js
+++ constants/queData.js
@@ -11,7 +11,7 @@ function QueData(readyTimer, eloGrowthPerSecond, baseEloRange, maxEloRange, minE
 
 const casualQueData = new QueData(PublicQueDatas['casual'].readyTimer, 7, 300, 2000, 800, 2300);
 
-const rankedQueData = new QueData(PublicQueDatas['ranked'].readyTimer, 0.5, 30, 300, 800, 2300);
+const rankedQueData = new QueData(PublicQueDatas['ranked'].readyTimer, 0.5, 40, 300, 800, 2300);
 
 export const queDatas = Object.freeze({ 
     'casual': casualQueData, 
